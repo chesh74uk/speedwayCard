b73d5f8a433a8eac470ae3be8693f3eb0af0f510
Chesh <chesh@chesh.info>
2018-06-24 11:46:51 +0000

    fixed scoring by meeting

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -4,26 +4,29 @@ module MeetingsHelper
        result = Heat.where(meeting_id:id).minimum("heat_time")
     end
     
-    
     def gate_results(id, gate_number)
-        heats = Heat.where(meeting_id:id)
         gate_score = 0
         gate_blue = 0
         gate_red = 0
         gate_white = 0
         gate_yellow = 0
+        heats = Heat.where(meeting_id:id)
+        
         heats.each do |heat|
-            blues = Blue.where(gate_number: gate_number)
-            reds = Red.where(gate_number: gate_number)
-            whites = White.where(gate_number: gate_number)
-            yellows = Yellow.where(gate_number: gate_number)
-            gate_blue = blues.select{|x| x[:gate_number] == gate_number}.map{|y| y[:score].to_i}.reduce(:+)
-            gate_red = reds.select{|x| x[:gate_number] == gate_number}.map{|y| y[:score].to_i}.reduce(:+)
-            gate_white = whites.select{|x| x[:gate_number] == gate_number}.map{|y| y[:score].to_i}.reduce(:+)
-            gate_yellow = yellows.select{|x| x[:gate_number] == gate_number}.map{|y| y[:score].to_i}.reduce(:+)
+            blue = Blue.where(gate_number: gate_number, heat_id: heat.id).sum(:score)
+            gate_blue += blue
+            red = Red.where(gate_number: gate_number, heat_id: heat.id).sum(:score)
+            gate_red += red
+            white = White.where(gate_number: gate_number, heat_id: heat.id).sum(:score)
+            gate_white += white
+            yellow = Yellow.where(gate_number: gate_number, heat_id: heat.id).sum(:score)
+            gate_yellow += yellow
         end
+        
         gate_score = gate_blue.to_i + gate_red.to_i + gate_white.to_i + gate_yellow.to_i
     end
+    
+    
     
     def meeting_home_score(id)
         heats = Heat.where(meeting_id:id)
@@ -62,10 +65,10 @@ module MeetingsHelper
         blue_score = 0
         red_score = 0
         heats.each do |heat|
-            blues = Blue.where(rider_name: rider)
-            reds = Red.where(rider_name: rider)
-            blue_score = blues.select{|x| x[:rider_name] == rider}.map{|y| y[:score].to_i}.reduce(:+)
-            red_score = reds.select{|x| x[:rider_name] == rider}.map{|y| y[:score].to_i}.reduce(:+)
+            blue = Blue.where(rider_name: rider, heat_id: heat.id).sum(:score)
+            blue_score += blue
+            red = Red.where(rider_name: rider, heat_id: heat.id).sum(:score)
+            red_score += red
         end
         total = blue_score.to_i + red_score.to_i
     end
@@ -77,10 +80,10 @@ module MeetingsHelper
         white_score = 0
         yellow_score = 0
         heats.each do |heat|
-            whites = White.where(rider_name: rider)
-            yellows = Yellow.where(rider_name: rider)
-            white_score = whites.select{|x| x[:rider_name] == rider}.map{|y| y[:score].to_i}.reduce(:+)
-            yellow_score = yellows.select{|x| x[:rider_name] == rider}.map{|y| y[:score].to_i}.reduce(:+)
+            white = White.where(rider_name: rider, heat_id: heat.id).sum(:score)
+            white_score += white
+            yellow = Yellow.where(rider_name: rider, heat_id: heat.id).sum(:score)
+            yellow_score += yellow
         end
         total = white_score.to_i + yellow_score.to_i
     end
